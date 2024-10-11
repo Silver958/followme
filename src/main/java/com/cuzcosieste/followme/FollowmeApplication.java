1f627d27ed7c9befbee1d5e56720a530d8f021dd
Silver <silvastar321@gmail.com>
2024-10-11 21:35:49 +0200

    Init

---
+++ src/main/java/com/cuzcosieste/followme/FollowmeApplication.java
@@ -0,0 +1,13 @@
+package com.cuzcosieste.followme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FollowmeApplication.class, args);
+	}
+
+}
